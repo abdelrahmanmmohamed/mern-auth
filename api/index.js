222ad1b950a245f143dd009651939fcc7b876725
abdelrahmanmohamed <abdelrahmanmohmed242@gmail.com>
2023-12-07 04:09:06 +0200

    connect to db

--- api/index.js
+++ api/index.js
@@ -1,7 +1,18 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 
+mongoose
+.connect(process.env.MONGO)
+.then(() =>{
+    console.log('connected to MongoDB');
+})
+.catch((err) =>{
+    console.log(err)
+});
 const app =express();
 
-app.listen(8000,() => {
-    console.console.log('server listing on port 3000');
+app.listen(3000,() => {
+    console.log('server listing on port 3000');
 });
